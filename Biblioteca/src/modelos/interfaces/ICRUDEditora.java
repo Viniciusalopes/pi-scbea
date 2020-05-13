ad3b00a06938193e7c3e208ad5b904248a72c6bb
viniciusalopes <suporte@viniciusalopes.com.br>
2020-05-12 23:58:44 -0300

    Projeto Inicial - Prof. Eugênio (Implementado em aula)

---
+++ Biblioteca/src/modelos/interfaces/ICRUDEditora.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos.interfaces;
+import modelos.classes.Editora;
+/**
+ *
+ * @author eugeniojulio
+ */
+public interface ICRUDEditora {
+    //Descrever os metodos
+   void incluir(Editora objeto);
+    
+}
