6126a585b5fe5a56d9abb49bab53b6e8734852c6
viniciusalopes <suporte@viniciusalopes.com.br>
2020-05-14 23:21:42 -0300

    Início interfaces

---
+++ Biblioteca/src/modelos/interfaces/IBdTxt.java
@@ -0,0 +1,14 @@
+/*
+* CRIAÇÃO: Vinicius Araujo Lopes
+* DATA   :
+
+*/
+package modelos.interfaces;
+
+/**
+ * Interface para manipulação de dados em arquivos de texto no formato .txt
+ * @author vovostudio
+ */
+public interface IBdTxt {
+    
+}
