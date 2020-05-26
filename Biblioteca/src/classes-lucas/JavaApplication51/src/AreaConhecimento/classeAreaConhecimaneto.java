2766a9bbc8f9a64bcb22bdafaaa95a1f1b4b66c0
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-26 20:01:50 -0300

    classes , configuraçao , AreaConhecimento

---
+++ Biblioteca/src/classes-lucas/JavaApplication51/src/AreaConhecimento/classeAreaConhecimaneto.java
@@ -0,0 +1,52 @@
+/*
+AreaConhecimento
+- idAreaConhecimento : int
+- cdd : int
+- descricaoAreaConhecimento : String
+-STERS--GETRS-
++ get() : void
++ set() : void
+-TO-STRING-
++ toString() : String
+ */
+package AreaConhecimento;
+
+/**
+ * ATRIBUTOS DA CLASSE
+ */
+public class classeAreaConhecimaneto {
+
+    private int idAreaConhecimento() {
+        idAreaConhecimento();
+        return 0;
+
+    }
+
+    private int cdd() {
+        cdd();
+        return 0;
+
+    }
+
+    private String descricaoAreaConhecimento() {
+        descricaoAreaConhecimento();
+        return null;
+
+    }
+
+//STERS & GETRS 
+    public void getAreaConhecimento() {
+
+    }
+
+    public void setAreaConhecimento() {
+
+    }
+//TO STRING 
+
+    @Override
+    public String toString() {
+        return "classeAreaConhecimaneto{" + '}';
+    }
+
+}
