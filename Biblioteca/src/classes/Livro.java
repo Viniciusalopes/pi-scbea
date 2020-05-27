de62ae02ecb3332159ccea36b7a49c091e354b01
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-27 20:08:40 -0300

    Livro

--- Biblioteca/src/classes/Livro.java
+++ Biblioteca/src/classes/Livro.java
@@ -1,14 +1,80 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Livro
+- idLivro : int
+- editora : Editora
+- autor : Autor
+- areaConhecimento : AreaConhecimento
+- titulo : String
+- descricaoLivro : String
+- isbn : String
+- anoPublicacao : int
+--GETERS--&--SETERS--
++ get() : void
++ set() : void
+---TO-SSTRING----
++ toString() : void
  */
 package classes;
 
 /**
- *
- * @author vovostudio
+ * CLASSE LIVRO
  */
 public class Livro {
-    
+
+    private int idLivro() {
+        idLivro();
+        return 0;
+
+    }
+
+    private editora Editora() {
+        Editora();
+        return Editora();
+
+    }
+
+    private autor Autor() {
+        Autor();
+        return Autor();
+
+    }
+
+    private areaConhecimento AreaConhecimento() {
+        AreaConhecimento();
+        return AreaConhecimento();
+
+    }
+
+    private String titulo() {
+        titulo();
+        return titulo();
+
+    }
+
+    private String descricaoLivro() {
+        descricaoLivro();
+        return descricaoLivro();
+
+    }
+
+    private String isbn() {
+        isbn();
+        return isbn();
+
+    }
+
+    private int anoPublicacao() {
+        anoPublicacao();
+        return 0;
+
+    }
+
+    public void getLivro() {
+
+    }
+
+    public void setLivro() {
+
+    }
+
 }
