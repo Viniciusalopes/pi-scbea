961eeac8f840377cdfbccea3753d00b349ba79c6
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-26 23:08:41 -0300

    Add files via upload

--- Biblioteca/src/classes/Emprestimo.java
+++ Biblioteca/src/classes/Emprestimo.java
@@ -1,14 +1,66 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+Emprestimo
+- idEmprestimo : int
+- exemplar : Exemplar
+- colaborador : Colaborador
+- dataEmprestimo : Date
+- dataDevolucao : Date
+- statusEmprestimo : EnumTipoStatus
+- valorMulta : int
++ get() : void
++ set() : void
++ toString() : void
+*/
 package classes;
 
 /**
- *
- * @author vovostudio
+classe emprestimo 
  */
 public class Emprestimo {
+    private int idEmprestimo(){
+        idEmprestimo();
+        return 0;
+        
+    }
+    private exemplar Exemplar(){
+        idEmprestimo();
+        return null;
+        
+    }
+    private colaborador  Colaborador(){
+        Colaborador();  
+        return null;
+      
+    }
+    private dataEmprestimo  Date(){
+        Date();
+        return null;
+    }
+    private dataDevolucao  Date(){
+        Date();
+        return null;
+    }
+    private statusEmprestimo  EnumTipoStatus(){
+        EnumTipoStatus();
+        return null;
+        
+    }
+    private int valorMulta(){
+        valorMulta();
+        return 0;
+        
+    }
+    public void getEmprestimo(){
+        
+    }
+     public void setEmprestimo(){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Emprestimo{" + '}';
+    }
+     
     
 }
