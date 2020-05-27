be595e94a8d07d93ea0bea2f783a32c72f2e7c22
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-26 22:29:26 -0300

    Editora

--- Biblioteca/src/classes/Editora.java
+++ Biblioteca/src/classes/Editora.java
@@ -1,14 +1,37 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+Editora
+- idEditora : int
+- nomeEditora : String
+- linhas : ArrayList<String>
+--GETERS-&-SETERS--
++ get() : void
++ set() : void
  */
 package classes;
 
 /**
- *
- * @author vovostudio
+CLASSE Editora
  */
 public class Editora {
+    public int idEditora(){
+        idEditora();
+        return 0;
+    }
+    public int nomeEditora(){
+        nomeEditora();
+        return 0;
+        
+    }
+    public void getEditora (){
+        
+    }
+    public void setEditora (){
+        
+    }
+
+    @Override
+    public String toString() {
+        return "Editora{" + '}';
+    }
     
 }
