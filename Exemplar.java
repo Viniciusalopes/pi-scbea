1a7ddfb25c85fbbbc24b51da6a28c5eb59cd7a40
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-29 20:29:06 -0300

    Exemplar
    Algumas correçoes

---
+++ Exemplar.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author vovostudio
+ */
+public class Exemplar {
+    //atributos 
+    //-------------------------------------
+    int idExemplar;
+    
+    //-------------------------------------
+    
+    //-------------------------------------
+    //contrutores 
+    public Exemplar(){
+        
+    }
+    
+    public Exemplar (Exemplar exemplar ){
+        idExemplar = exemplar.idExemplar;
+    }
+    
+    public Exemplar (int idExemplar){
+        this.idExemplar = idExemplar;
+    }
+    //-------------------------------------
+    //geters e seters 
+    //-------------------------------------
+    public int getIdExemplar() {
+        return idExemplar;
+    }
+
+    public void setIdExemplar(int idExemplar) {
+        this.idExemplar = idExemplar;
+    }
+    //-------------------------------------
+    
+    //-------------------------------------
+    //toString 
+    @Override 
+    public String toString(){
+        return String.format("%d", idExemplar);
+    }
+    
+
+}
