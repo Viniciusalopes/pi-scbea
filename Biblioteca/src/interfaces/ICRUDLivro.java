8d7a7364fcb28f4d87edbeec8abea0db085b3e93
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-05-31 22:39:22 -0300

    ICRUDE Livro

--- Biblioteca/src/interfaces/ICRUDLivro.java
+++ Biblioteca/src/interfaces/ICRUDLivro.java
@@ -1,14 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+=====================================
+ICRUDObjeto
+--------METODOS----------------------
++ listar() : ArrayList<Objeto>
++ incluir(objeto : Objeto) : void
++ atualizar(objeto : Objeto) : void
++ excluir(idObjeto : int) : void
+-------------------------------------
+-------------------------------------
  */
 package interfaces;
 
+import classes.Livro;
+import java.util.ArrayList;
+
 /**
  *
- * @author vovostudio
+ * @author lucas
  */
 public interface ICRUDLivro {
-    
+
+    ArrayList<Livro> listar() throws Exception;
+
+    void incluir(Livro livro) throws Exception;
+
+    void alterar(Livro livro) throws Exception;
+
+    void excluir(Livro livro) throws Exception;
 }
