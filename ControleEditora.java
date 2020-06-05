889435d89def8be6a33a9911091efc023864d317
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-06-04 21:33:22 -0300

    persistencia e icrud editora concluida

---
+++ ControleEditora.java
@@ -0,0 +1,44 @@
+/*
+=====================================
+ICRUDObjeto
+--------METODOS----------------------
++ listar() : ArrayList<Objeto>
++ incluir(objeto : Objeto) : void
++ alterar(objeto : Objeto) : void
++ excluir(idObjeto : int) : void
+-------------------------------------
+-------------------------------------
+ */
+package controle;
+
+import classes.Editora;
+import interfaces.ICRUDEditora;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author lucas
+ */
+public class ControleEditora implements ICRUDEditora {
+
+    @Override
+    public ArrayList<Editora> listar() throws Exception {//ok
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void incluir(Editora editora) throws Exception {//ok
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterar(Editora editora) throws Exception {//ok
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(int idEditora) throws Exception {//ok
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
