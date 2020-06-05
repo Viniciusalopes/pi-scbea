9fefce21865b94b648378a0b3b07e5b27cebf17b
marcosjob369 <57804223+marcosjob369@users.noreply.github.com>
2020-06-04 21:32:03 -0300

    padronização de métodos

--- Biblioteca/src/interfaces/ICRUDAreaConhecimento.java
+++ Biblioteca/src/interfaces/ICRUDAreaConhecimento.java
@@ -4,9 +4,15 @@ import classes.AreaConhecimento;
 import java.util.ArrayList;
 
 public interface ICRUDAreaConhecimento {
+
     ArrayList<AreaConhecimento> listar() throws Exception;
-    void incluir(AreaConhecimento areaConhecimento) throws Exception;
-    void alterar(AreaConhecimento areaConhecimento) throws Exception;
-    void excluir(int idAreaConhecimento) throws Exception;
+
     AreaConhecimento buscarPeloId(int idAreaConhecimento) throws Exception;
+
+    void incluir(AreaConhecimento areaConhecimento) throws Exception;
+
+    void alterar(AreaConhecimento areaConhecimento) throws Exception;
+
+    void excluir(int idAreaConhecimento) throws Exception;
+
 }
