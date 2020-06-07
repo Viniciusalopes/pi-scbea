c1c50f63805dea1aad2faedb8fa3e63e1d2539e2
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-06-07 20:40:49 -0300

    pre-concluida

--- Biblioteca/src/controle/ControleExemplar.java
+++ Biblioteca/src/controle/ControleExemplar.java
@@ -5,8 +5,12 @@
  */
 package controle;
 
+import classes.AreaConhecimento;
+import classes.Emprestimo;
 import classes.Exemplar;
+import interfaces.ICRUDEmprestimo;
 import interfaces.ICRUDExemplar;
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import persistencia.PersistenciaExemplar;
 
@@ -19,34 +23,49 @@ public class ControleExemplar implements ICRUDExemplar {
     private ArrayList<Exemplar> colecao;
 
     public ControleExemplar() throws Exception {
-        persistencia = new PersistenciaExemplar();
+        persistencia = new PersistenciaExemplar() {};
         this.colecao = colecao;
     }
     
 
     @Override
     public ArrayList<Exemplar> listar() throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     return persistencia.listar();
     }
 
     @Override
     public Exemplar buscarPeloId(int idExmplar) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+             return persistencia.buscarPeloId(idExmplar);
+
     }
 
     @Override
     public void incluir(Exemplar exemplar) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        colecao = listar();
+        persistencia.incluir(exemplar);
     }
 
     @Override
     public void alterar(Exemplar exemplar) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+     colecao = listar();
+     persistencia.alterar(exemplar);
     }
 
     @Override
-    public void excluir(Exemplar exemplar) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public void excluir(int idExemplar) throws Exception {
+        ICRUDEmprestimo controleEmprestimo = new ControleEmprestimo();
+        
+        ArrayList<Emprestimo> emprestimos = controleEmprestimo.listar();
+        //se o livro ja foi alguma vez emprestado nao pode ser excluido 
+        for (Emprestimo emprestimo : emprestimos) {
+            if (emprestimo.getExemplar().getIdExemplar() == idExemplar) {
+                throw new Exception("este exemplar já foi emprestado por isso não pode ser excluido! ");
+                
+                
+            }
+        }
+        persistencia.excluir(idExemplar);
     }
-
 }
+
+
