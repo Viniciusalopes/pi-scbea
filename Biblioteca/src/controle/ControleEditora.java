5cd931b09beca98863291da056dfc545eff73bb3
lucas araujo da silva <55209015+lucasDEV20@users.noreply.github.com>
2020-06-07 22:05:10 -0300

    finaliizando controle editora com o vovos2

--- Biblioteca/src/controle/ControleEditora.java
+++ Biblioteca/src/controle/ControleEditora.java
@@ -14,36 +14,62 @@ package controle;
 import classes.Editora;
 import interfaces.ICRUDEditora;
 import java.util.ArrayList;
+import persistencia.PersistenciaEditora;
 
 /**
  *
  * @author lucas
  */
 public class ControleEditora implements ICRUDEditora {
+     private ICRUDEditora persistencia ;
+     private ArrayList<Editora> colecao;
 
+    public ControleEditora() throws Exception {
+        persistencia = new PersistenciaEditora();
+        colecao = new ArrayList<>();
+    }
+     
+     
+    
     @Override
     public ArrayList<Editora> listar() throws Exception {//ok
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    return persistencia.listar();
+    
     }
 
     @Override
-    public Editora buscarPeloId(int id) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    public Editora buscarPeloId(int idEditora) throws Exception {
+        return persistencia.buscarPeloId(idEditora);
     }
 
     @Override
     public void incluir(Editora editora) throws Exception {//ok
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+         if (editora.getNomeEditora().trim().length() < 2) {
+            throw new Exception("Nome não pode ficar em branco!");
+        }
+
+        if (editora.getNomeEditora().toLowerCase().charAt(0) == editora.getNomeEditora().toLowerCase().charAt(1)) {
+            throw new Exception("Nome com duas letras , e ainda iguais?? ta de brinks...");
+        }
+         colecao = persistencia.listar();
+        for (Editora objAutor : colecao) {
+            if (objAutor.getNomeEditora().equals(editora.getNomeEditora())) {
+                throw new Exception("Já existe um autor cadastrado com este nome!");
+            }
+        }
+
+        persistencia.incluir(editora);
     }
 
     @Override
     public void alterar(Editora editora) throws Exception {//ok
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       colecao = listar(); 
+       persistencia.alterar(editora);
     }
 
     @Override
     public void excluir(int idEditora) throws Exception {//ok
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+      
     }
 
 }
