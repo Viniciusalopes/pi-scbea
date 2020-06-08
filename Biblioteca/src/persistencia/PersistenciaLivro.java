4aead5c40f68a91ecc1f0fcb933ad1d9ea53d2d4
jotacabral <57968606+jotacabral@users.noreply.github.com>
2020-06-07 23:50:22 -0300

    Correção do parametro de excluir.

--- Biblioteca/src/persistencia/PersistenciaLivro.java
+++ Biblioteca/src/persistencia/PersistenciaLivro.java
@@ -5,10 +5,39 @@
  */
 package persistencia;
 
+import classes.Livro;
+import interfaces.ICRUDLivro;
+import java.util.ArrayList;
+
 /**
  *
  * @author vovostudio
  */
-public class PersistenciaLivro {
+public class PersistenciaLivro implements ICRUDLivro {
+
+    @Override
+    public ArrayList<Livro> listar() throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Livro buscarPeloId(int id) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void incluir(Livro livro) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void alterar(Livro livro) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void excluir(int idLivro) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
